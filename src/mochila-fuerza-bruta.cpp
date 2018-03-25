bbef7db636af74d8aaf5edd1ef6994143435f7bc
nsm <smnicolas>
2018-03-25 16:51:19 -0300

    Uso pares en vez de 2 vectores ara ps y ws

--- src/mochila-fuerza-bruta.cpp
+++ src/mochila-fuerza-bruta.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
 #include <vector>
 #include <numeric>
+#include <utility>
 
 using namespace std;
+void
+resolver_fuerza_bruta2 (vector<int> &pre, vector<int> &conj, int W,
+                        pair<int,int> &totales,
+                        vector<pair<int,int>> &items,
+                        vector<int> &beneficios) ;
+
 void
 resolver_fuerza_bruta(vector<int> &pre, vector<int> &conj,
                       int W, int peso_total,
@@ -15,8 +22,26 @@ int calcular_suma(vector<int>&ps,vector<int>&conj) {
      return res;
 }
 
-int fuerza_bruta (vector<int> precios, vector<int>weights, int W) {
 
+// obtiene peso y beneficio de un subconjunto conj de items
+// O(n) , n == len(conj)
+pair<int,int> calcular_sumas(vector<pair<int,int>>&items, vector<int>&conj) {
+     pair<int,int> res(0,0);
+     for (int& x : conj ) {
+          res.first += items[x].first; 
+          res.second += items[x].second;
+     }
+     return res;
+}
+
+
+int calcular_precio(vector<pair<int,int>>&items, vector<int>&conj) {
+     int res = 0;
+     for (int& x : conj ) { res += items[x].first; }
+     return res;
+}
+
+int fuerza_bruta (vector<int> precios, vector<int>weights, int W) {
      vector<int> pre;
      vector<int> conj(weights.size());
      vector<int> beneficios;
@@ -24,8 +49,7 @@ int fuerza_bruta (vector<int> precios, vector<int>weights, int W) {
      int precio_total = 0;
      iota(conj.begin(), conj.end(), 0);
      for (int x : weights) { peso_total += x; }
-     resolver_fuerza_bruta(pre, conj, W, peso_total, precios, weights,
-                           beneficios);
+     resolver_fuerza_bruta(pre, conj, W, peso_total, precios, weights, beneficios);
      for (int x : precios) { precio_total += x; }
      int precio_sacado = precio_total;
 
@@ -37,6 +61,27 @@ int fuerza_bruta (vector<int> precios, vector<int>weights, int W) {
      return precio_total - precio_sacado;
      
 }
+
+int fuerza_bruta2 (vector<pair<int,int>> items, int W) {
+
+     vector<int> pre;
+     vector<int> conj(items.size());
+     vector<int> beneficios;
+     iota(conj.begin(), conj.end(), 0);
+     pair<int, int> totales = calcular_sumas(items, conj);
+
+     resolver_fuerza_bruta2(pre, conj, W, totales, items, beneficios);
+
+     int precio_sacado = totales.first;
+     for (int x : beneficios) {
+          if (precio_sacado > x) 
+               precio_sacado = x;
+     }
+     return totales.first - precio_sacado;
+     
+}
+
+
 
 int print_vec(vector<int> v, string str) {
      int suma = 0;
@@ -85,5 +130,49 @@ resolver_fuerza_bruta (vector<int> &pre, vector<int> &conj,
           return;
      resolver_fuerza_bruta(pre_copia, conj_copia,
                            W, peso_total, ps, ws, beneficios);
+     return;
+}
+
+
+
+void
+resolver_fuerza_bruta2 (vector<int> &pre, vector<int> &conj, int W,
+                        pair<int,int> &totales,
+                        vector<pair<int,int>> &items,
+                        vector<int> &beneficios) {
+
+     pair<int,int> suma_pre = calcular_sumas(items, pre);
+     if (conj.size() == 1) {
+
+          pair<int,int> suma_pre_conj= calcular_sumas(items, conj);
+          suma_pre_conj.first += suma_pre.first;
+          suma_pre_conj.second += suma_pre.second;
+          
+          // solo vale la solución si lo que saco pesa suficiente como
+          // para que lo que queda no supere W
+          if (W < totales.second - suma_pre_conj.second) 
+               return;
+          beneficios.push_back(suma_pre_conj.first);
+
+          if (W < totales.second - suma_pre.second)
+               return;
+          beneficios.push_back(suma_pre.first);
+          return; 
+     }
+     vector<int> pre_copia(pre);
+     //vector<int> pre_0(pre);
+     vector<int> conj_copia(conj);
+     int head = conj.back();
+     pre_copia.push_back(head);
+     conj_copia.pop_back();
+
+     resolver_fuerza_bruta2(
+          pre, conj_copia, W, totales, items, beneficios);
+
+     // si ya saque suficiente para obtener una solucion, no sigo sacando
+     if (totales.second - suma_pre.second <= W)
+          return;
+     resolver_fuerza_bruta2(
+          pre_copia, conj_copia, W, totales, items, beneficios);
      return;
 }
--- src/mochila.cpp
+++ src/mochila.cpp
@@ -1,8 +1,9 @@
 #include <iostream>
 #include <vector>
 #include <numeric>
-
+#include <random>
 #include "mochila.h"
+#include <utility>
 
 using namespace std;
 
@@ -10,18 +11,32 @@ int calcular_beneficio(vector<int>&ps,vector<int>&conj);
 int print_vec(vector<int> v, string str) ;
 
 int fuerza_bruta (vector<int> precios, vector<int>weights, int W) ;
+int fuerza_bruta2 (vector<pair<int,int>> items, int W) ;
+
+vector<int> random_int_vector(int size);
+
+void copiar_items(vector<pair<int,int>> &items,
+                  vector<int> &ps, vector<int> &ws);
+vector<pair<int,int>> random_items(int cantidad) ;
 
 using namespace std;
 int main (int argc, char ** argv) {
-     int n = 5;
-     int W = 100;
-     
-     vector<int> ps(n);
-     vector<int> ws(n);
 
-     ps = { 4 , 8, 10, 2, 11, 6, 8, 3, 8, 6, 11, 21, 32};
-     ws = { 9 , 1, 20, 7, 2, 10, 8, 9, 3, 9, 7, 33, 43};
+     int n = 15;
+     int W = 100;
+
+     if (argc > 1)
+          n = atoi(argv[1]);
      
+     vector<int> ps;
+     vector<int> ws;
+     vector<pair<int,int>> items = random_items(n);
+     copiar_items(items, ps, ws);
+     // ps = { 4 , 8, 10, 2, 11, 6, 8, 3, 8, 6, 11, 21, 32};
+     // ws = { 9 , 1, 20, 7, 2, 10, 8, 9, 3, 9, 7, 33, 43};
+
+     // ps = random_int_vector(n);
+     // ws = random_int_vector(n);
      cout << "n es " << n << " W es " << W << endl;
      int suma_precios = print_vec(ps, "precios: ");
      cout << "\tprecio total: " << suma_precios << endl;
@@ -30,10 +45,51 @@ int main (int argc, char ** argv) {
 
      int res = fuerza_bruta(ps, ws, W);
      cout << res << endl;
+
+     // cout << "random vector de n: " << n << endl;
+     // vector<int> data = random_int_vector(n);
+     // for (int x : data) { cout << x << " "; }
+     // cout << endl;
+
+
+     int res2 = fuerza_bruta2(items, W);
+     cout << "con pares: " << res2 << endl;
+
 }
 
-namespace mochila {
-     
-     
-     
+
+vector<int> random_int_vector(int size) {
+     default_random_engine generator;
+     generator.seed(random_device()());
+     uniform_int_distribution<int>
+          distribution(1, 100);
+     //auto cubilete = bind ( distribution, generator );
+     vector<int> data;
+     for (int i = 0; i < size; i++) {
+          data.push_back(distribution(generator));
+     }
+     return data;
+}
+
+void copiar_items(vector<pair<int,int>> &items,
+                  vector<int> &ps, vector<int> &ws) {
+     for (pair<int,int> x : items) {
+          ps.push_back(x.first);
+          ws.push_back(x.second);
+     }
+
+}
+
+vector<pair<int,int>> random_items(int cantidad) {
+     default_random_engine generator;
+     generator.seed(random_device()());
+     uniform_int_distribution<int>
+          distribution(1, 100);
+     //auto cubilete = bind ( distribution, generator );
+     vector<pair<int,int>> data;
+     for (int i = 0; i < cantidad; i++) {
+          data.push_back(make_pair(distribution(generator),
+                                   distribution(generator)));
+     }
+     return data;
 }
