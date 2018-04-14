8a2d8f753821d22eda7e843dfabdadf13bd6fa18
nsm <smnicolas>
2018-04-14 11:51:42 -0300

    emprolijo fb

--- src/mochila-fuerza-bruta.cpp
+++ src/mochila-fuerza-bruta.cpp
@@ -7,41 +7,29 @@ using namespace std;
 
 
 void
-resolver_fuerza_bruta (vector<item_t> &items, int i, int W, mochila &m,
-                       int &menor_beneficio_sacado);
+resolver_fuerza_bruta (vector<item_t> &items, int i, int W,item_t &m,
+                       int &solucion);
 
 int fuerza_bruta (vector<item_t> &items, int W) {
-     mochila m;
-     m.afuera = item_t(0,0);
-     m.adentro = item_t(0,0);
-     int menor_beneficio_sacado, beneficio_total = 0;
-
-     for (int i = 0; i < items.size(); i ++) 
-          beneficio_total += items[i].first;
-     menor_beneficio_sacado = beneficio_total;
-     resolver_fuerza_bruta(items, items.size(), W, m, menor_beneficio_sacado);
-
-     return beneficio_total - menor_beneficio_sacado;
+     item_t m = item_t(0,0);
+     int solucion = 0;
+     resolver_fuerza_bruta(items, items.size(), W, m, solucion);
+     return solucion;
 }
 
-
 void
-resolver_fuerza_bruta (vector<item_t> &items, int i, int W, mochila &m,
-                       int &menor_beneficio_sacado)
+resolver_fuerza_bruta (vector<item_t> &items, int i, int W, item_t &m,
+                       int &solucion)
 {
-     if (i == 0) {// ultimo
-          if (m.afuera.first < menor_beneficio_sacado &&
-              m.adentro.second <= W)
-               menor_beneficio_sacado  = m.afuera.first;
-     } else {
-          mochila m_sin_iesimo(m);
-          agregar_item_a_suma(m.adentro, items[i - 1]);
-          resolver_fuerza_bruta(items, i - 1, W, m,
-                                menor_beneficio_sacado);
-          agregar_item_a_suma(m_sin_iesimo.afuera, items[i - 1]);
-          resolver_fuerza_bruta(items, i - 1, W, m_sin_iesimo,
-                                menor_beneficio_sacado);
+     if (i > 0) {
+          item_t m_sin_iesimo(m);
+          agregar_item_a_suma(m, items[i - 1]);
+          resolver_fuerza_bruta(items, i - 1, W, m, solucion);
+          resolver_fuerza_bruta(items, i - 1, W, m_sin_iesimo, solucion);
+     } else if (m.first > solucion && m.second <= W) {
+               solucion  = m.first;
      }
+
 }
 
 
